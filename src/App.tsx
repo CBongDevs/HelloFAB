f869612bd45d61e014d1c7c5f877185ba39f951a
Andrew Thompson <mrwerdo331@me.com>
2020-06-24 13:22:21 +1000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface AppState {
+  status: string;
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = { status: '' };
+  }
+
+  async buttonClick() {
+    let request = await fetch('/api/v2/ping');
+    let json = await request.json();
+    this.setState({
+      'status': json['time']
+    });
+  }
+
+  render(): JSX.Element {
+    return (
+    <div onClick={this.buttonClick.bind(this)} className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p key={Date.now()} id='status'>
+          {this.state.status}
+        </p>
+      </header>
+    </div>)
+  }
+}
+
+export default App;
